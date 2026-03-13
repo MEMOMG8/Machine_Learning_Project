113a246dce4aa88ee311496f0c92c97ead4e8818
MEMOMG8 <manuelperezg20@gmail.com>
2026-03-13 12:24:35 -0500

    Movie recommender system

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
 
 movies = pickle.load(open('movies.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
 
+st.subheader("Find movies similar to your favorites using Machine Learning")
+
+def fetch_poster(movie_id):
+
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=6b9c27cb7eb5e71c51aba688147911b5&language=en-US"
+
+    data = requests.get(url)
+    data = data.json()
+
+    poster_path = data['poster_path']
+
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+
+    return full_path
 
 def recommend(movie):
 
@@ -15,23 +30,46 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
+    recommended_posters = []
 
     for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
 
-    return recommended_movies
+    return recommended_movies, recommended_posters
 
 
 st.title('Movie Recommender System')
 
 selected_movie = st.selectbox(
-    "Choose a movie",
-    movies['title'].values
+    "Search a movie",
+    movies['title'].sort_values().values
 )
 
 if st.button('Recommend'):
+    with st.spinner("Finding similar movies..."):
+    
+        names, posters = recommend(selected_movie)
 
-    recommendations = recommend(selected_movie)
+        col1, col2, col3, col4, col5 = st.columns(5)
 
-    for movie in recommendations:
-        st.write(movie)
+        with col1:
+            st.image(posters[0], use_container_width=True)
+            st.caption(names[0])
+
+        with col2:
+            st.image(posters[1], use_container_width=True)
+            st.caption(names[1])
+
+        with col3:
+            st.image(posters[2], use_container_width=True)
+            st.caption(names[2])
+
+        with col4:
+            st.image(posters[3], use_container_width=True)
+            st.caption(names[3])
+
+        with col5:
+            st.image(posters[4], use_container_width=True)
+            st.caption(names[4])
